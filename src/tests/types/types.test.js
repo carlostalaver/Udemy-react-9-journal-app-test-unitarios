12273354256726bebf8e356da7d2b3dddfe7b6d0
ctalavera <ctalavera@bch.bancodechile.cl>
2022-01-06 21:58:37 -0300

    first commit

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,31 @@
+import { types } from "../../types/types"
+
+
+describe('Pruebas en el objeto types', () => {
+    test('debe de ser igual a ', () => {
+
+        expect( types ).toEqual({
+
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+        
+            uiSetError: '[UI] Set Error',
+            uiRemoveError: '[UI] Remove Error',
+        
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+        
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Updated note',
+            notesFileUrl: '[Notes] Updated image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout Cleaning',
+        
+        })
+        
+    })
+    
+    
+})
